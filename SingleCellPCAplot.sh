a3de3397c7e7082f49dc7805668d7690dd929a85
Milos Pjanic <pjanic.milos@gmail.com>
2017-09-15 14:59:41 -0700

    Update SingleCellPCAplot.sh

--- SingleCellPCAplot.sh
+++ SingleCellPCAplot.sh
@@ -15,11 +15,18 @@ x1[numc]<- lapply(x1[numc], function(x) as.numeric(as.character(x)))
 
 pca= prcomp( x1 , center=T, scale=T)
 
-#plot simple pca
+#plot simple pca 1/2
 
-pdf("pca_nocol.pdf")
+pdf("pca_nocol.1.2.pdf")
 plot(pca$rotation[,1],pca$rotation[,2], xlab = \"PC1\", ylab = \"PC2\")
 text(pca$rotation[,1],pca$rotation[,2], row.names(pca$rotation), cex=0.2, pos=4)
+dev.off()
+
+#plot simple pca 2/3
+
+pdf("pca_nocol.2.3.pdf")
+plot(pca$rotation[,2],pca$rotation[,3], xlab = \"PC2\", ylab = \"PC3\")
+text(pca$rotation[,2],pca$rotation[,3], row.names(pca$rotation), cex=0.2, pos=4)
 dev.off()
 
 #select row with the gene of interest (second parameter), melt, assign rownames
@@ -34,36 +41,50 @@ x3<-cbind(pca$rotation,x2)
 x3$color=\"grey\"
 
 #assign 3 color scheme
-x3$color[x3$value<range(x3$value)[2]/10 & x3$value>0] ="lavenderblush1"
-x3$color[x3$value<(range(x3$value)[2]/10)*2 & x3$value>range(x3$value)[2]/10] ="lightpink"
-x3$color[x3$value<(range(x3$value)[2]/10)*3 & x3$value>(range(x3$value)[2]/10)*2] ="lightpink1"
+x3$color[x3$value<range(x3$value)[2]/3 & x3$value>0] =\"#FFAAAA\"
+x3$color[x3$value<(range(x3$value)[2]/3)*2 & x3$value>range(x3$value)[2]/3] =\"#FF5555\"
+x3$color[x3$value<(range(x3$value)[2]/3)*3 & x3$value>(range(x3$value)[2]/3)*2] =\"#FF0000\"
 
-pdf("pca.pdf")
-plot(pca$rotation[,1],pca$rotation[,2], col=x3$color, xlab = "PC1", ylab = "PC2", pch=19)
+#plot pca 1/2 with 3 colors in a gradient
+
+pdf(\"pca.3col.1.2.pdf\")
+plot(pca$rotation[,1],pca$rotation[,2], col=x3$color, xlab = \"PC1\", ylab = \"PC2\", pch=19)
 text(pca$rotation[,1],pca$rotation[,2], row.names(pca$rotation), cex=0.2, pos=4)
 dev.off()
 
-x3$color="grey96"
-x3$color[x3$value<range(x3$value)[2]/10 & x3$value>0] ="lavenderblush1"
-x3$color[x3$value<(range(x3$value)[2]/10)*2 & x3$value>range(x3$value)[2]/10] ="lightpink"
-x3$color[x3$value<(range(x3$value)[2]/10)*3 & x3$value>(range(x3$value)[2]/10)*2] ="lightpink1"
-x3$color[x3$value<(range(x3$value)[2]/10)*4 & x3$value>(range(x3$value)[2]/10)*3] ="lightpink2"
-x3$color[x3$value<(range(x3$value)[2]/10)*5 & x3$value>(range(x3$value)[2]/10)*4] ="lightpink3"
-x3$color[x3$value<(range(x3$value)[2]/10)*6 & x3$value>(range(x3$value)[2]/10)*5] ="orangered3"
-x3$color[x3$value<(range(x3$value)[2]/10)*7 & x3$value>(range(x3$value)[2]/10)*6] ="red3"
-x3$color[x3$value<(range(x3$value)[2]/10)*8 & x3$value>(range(x3$value)[2]/10)*7] ="red2"
-x3$color[x3$value<(range(x3$value)[2]/10)*9 & x3$value>(range(x3$value)[2]/10)*8] ="red1"
-x3$color[x3$value<(range(x3$value)[2]/10)*10 & x3$value>(range(x3$value)[2]/10)*9] ="red"
+#plot pca 2/3 with 3 colors in a gradient
 
-data<- read.table('sample_data.txtt', header=TRUE, row.name=1)
-# Create new column filled with default colour
-data$Colour="black"
-# Set new column values to appropriate colours
-data$Colour[data$col_name2>=3]="red"
-data$Colour[data$col_name2<=1]="blue"
-# Plot all points at once, using newly generated colours
-plot(data$col_name1,data$col_name2, ylim=c(0,5), col=data$Colour, ylim=c(0,10))
+pdf(\"pca.3col.2.3.pdf\")
+plot(pca$rotation[,2],pca$rotation[,3], col=x3$color, xlab = \"PC2\", ylab = \"PC3\", pch=19)
+text(pca$rotation[,2],pca$rotation[,3], row.names(pca$rotation), cex=0.2, pos=4)
+dev.off()
 
+x3$color=\"grey96\"
+x3$color[x3$value<range(x3$value)[2]/10 & x3$value>0] =\"#FFE5E5\"
+x3$color[x3$value<(range(x3$value)[2]/10)*2 & x3$value>range(x3$value)[2]/10] =\"#FFCCCC\"
+x3$color[x3$value<(range(x3$value)[2]/10)*3 & x3$value>(range(x3$value)[2]/10)*2] =\"#FFB2B2\"
+x3$color[x3$value<(range(x3$value)[2]/10)*4 & x3$value>(range(x3$value)[2]/10)*3] =\"#FF9999\"
+x3$color[x3$value<(range(x3$value)[2]/10)*5 & x3$value>(range(x3$value)[2]/10)*4] =\"#FF7F7F\"
+x3$color[x3$value<(range(x3$value)[2]/10)*6 & x3$value>(range(x3$value)[2]/10)*5] =\"#FF6666\"
+x3$color[x3$value<(range(x3$value)[2]/10)*7 & x3$value>(range(x3$value)[2]/10)*6] =\"#FF4C4C\"
+x3$color[x3$value<(range(x3$value)[2]/10)*8 & x3$value>(range(x3$value)[2]/10)*7] =\"#FF3333\"
+x3$color[x3$value<(range(x3$value)[2]/10)*9 & x3$value>(range(x3$value)[2]/10)*8] =\"#FF1919\"
+x3$color[x3$value<(range(x3$value)[2]/10)*10 & x3$value>(range(x3$value)[2]/10)*9] =\"#FF0000\"
+
+
+#plot pca 1/2 with 10 colors in a gradient
+
+pdf(\"pca.10col.1.2.pdf\")
+plot(pca$rotation[,1],pca$rotation[,2], col=x3$color, xlab = \"PC1\", ylab = \"PC2\", pch=19)
+text(pca$rotation[,1],pca$rotation[,2], row.names(pca$rotation), cex=0.2, pos=4)
+dev.off()
+
+#plot pca 2/3 with 10 colors in a gradient
+
+pdf(\"pca.10col.2.3.pdf\")
+plot(pca$rotation[,2],pca$rotation[,3], col=x3$color, xlab = \"PC2\", ylab = \"PC3\", pch=19)
+text(pca$rotation[,2],pca$rotation[,3], row.names(pca$rotation), cex=0.2, pos=4)
+dev.off()
 
 " > script.r
 
